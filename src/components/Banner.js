7864ea0fd4aa96e99d36ac0171023e420d91a279
apprenti_développeur <xd.decaudin@gmail.com>
2023-06-16 12:50:14 +0200

    Début du développement

---
+++ src/components/Banner.js
@@ -0,0 +1,11 @@
+import logo from "../assets/logo.png";
+
+const Banner = () => {
+  return (
+    <div className="logo">
+      <img src={logo} alt="logo de Kasa" className="kasa-logo" />
+    </div>
+  );
+};
+
+export default Banner;
